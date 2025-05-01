733ede4ae7f1da16fc81c977076612cd2a6991a0
pablooglez <pablogon@student.42malaga.com>
2025-05-01 21:25:13 +0200

    CPP07

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pablogon <pablogon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 13:47:23 by pablogon          #+#    #+#             */
-/*   Updated: 2025/05/01 13:53:07 by pablogon         ###   ########.fr       */
+/*   Updated: 2025/05/01 14:20:35 by pablogon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 int	main()
 {
-	
+	int a = 2;
+	int b = 3;
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	::swap(a, b);
+	std::cout << "Después de swap: a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "Después de swap: c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+	std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
+
+	return (0);
 }
